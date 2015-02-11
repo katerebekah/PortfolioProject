d6a0920a706e04aee61fdae3f90275cea040b1c0
Kate <katerebekah@gmail.com>
2015-02-11 15:31:29 -0600

    Quick fix to more accurately reflect weather condition possibilities.

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,29 @@
-var currentWeather = "";
 $.ajax({
 	url: "http://api.wunderground.com/api/33c41345d1292535/conditions/q/TN/Nashville.json",
 	datatype: 'jsonp',
 	success: function(results) {
-		currentWeather = results.current_observation.weather;
+		var currentWeather = results.current_observation.weather;
 		$('.weatherNashville').text(currentWeather);
-		if (currentWeather === "Mostly Cloudy" || currentWeather === "Mist") {
+		if (currentWeather === "Heavy Fog" || currentWeather === "Light Fog" 
+			|| currentWeather === "Patches of Fog" || currentWeather === "Fog" || currentWeather === "Mist") {
 		   $('body').addClass("fog");
 		}
-		else if (currentWeather === "Sunny" || currentWeather === "Fair") {
+		else if (currentWeather === "Sunny" || currentWeather === "Fair" || currentWeather === "Clear") {
 			$('body').addClass("sunny");
 		}
-		else if (currentWeather === "Snow") {
+		else if (currentWeather === "Heavy Snow Showers" || currentWeather === "Light Snow Showers" 
+				|| currentWeather === "Snow Showers" || currentWeather === "Blowing Snow" 
+				|| currentWeather === "Low Drifting Snow" || currentWeather === "Heavy Snow" 
+				|| currentWeather === "Snow" || currentWeather === "Light Snow") {
 			$('body').addClass("snow");
 		}
-		else if (currentWeather === "Thunderstorm") {
+		else if (currentWeather === "Thunderstorm and Rain" || currentWeather === "Thunderstorm") {
 			$('body').addClass("stormy");
 		}
-		else if (currentWeather === "Rain" || currentWeather === "Showers" || currentWeather === "Scattered Showers") {
+		else if (currentWeather === "Freezing Rain" || currentWeather === "Heavy Rain" 
+				|| currentWeather === "Light Rain" || currentWeather === "Heavy Drizzle" 
+				|| currentWeather === "Light Drizzle" || currentWeather === "Rain" 
+				|| currentWeather === "Showers" || currentWeather === "Scattered Showers") {
 			$('body').addClass("rainy");
 		}
 		else {
